f9e809da662f9c6e4b55affa6ca642235fd9667c
zenithYP26 <3538396090@qq.com>
2024-06-29 00:49:59 +0800

    确保在 platform.h 中只声明函数，而在 platform.c 中定义这些函数。

---
+++ platform.c
@@ -0,0 +1,20 @@
+#include "platform.h"
+#include "log.h"
+#include <WinSock2.h>
+
+#ifdef _WIN32
+
+unsigned socket_init() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        log_error("WSAStartup failed");
+        return 0;
+    }
+    return 1;
+}
+
+void socket_cleanup() {
+    WSACleanup();
+}
+
+#endif
--- platform.h
+++ platform.h
@@ -4,29 +4,22 @@
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
-unsigned socket_init() {
-    WSADATA wsaData;
-    // 初始化 Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-       log_error("WSAStartup failed");
-       return 0;
-    }
-    return 1;
-}
+#include <windows.h>
 
-void socket_cleanup() {
-    // 清理 Winsock
-    WSACleanup();
-}
+unsigned socket_init();
+void socket_cleanup();
+
 #else
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
 #define closesocket close
 #define SOCKET int
 #define SOCKET_ERROR -1
 #define INVALID_SOCKET -1
+
 #define socket_init() // 在 Linux 上，这个宏为空
 #define socket_cleanup()
 #endif
